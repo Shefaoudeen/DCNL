3f4dfda0367e1794983e6b8420d1cbfce8f43843
Shefa <133137439+Shefaoudeen@users.noreply.github.com>
2024-07-18 21:30:44 +0530

    Update 7CLI.c

--- SEM/7CLI.c
+++ SEM/7CLI.c
@@ -11,21 +11,21 @@
 void func(int connfd){
     char buff[MAX];
     int n;
-    while(1){
-        //BUFFER INITIALIZATION
-        bzero(buff,MAX);
-        read(connfd,buff,sizeof(buff)); //READ
-        printf("\nFrom Client : %s \nTo Client : ",buff);
-        bzero(buff,MAX);
-        n=0;
-        while((buff[n++]==getchar())!='\n'); 
-        
-        write(connfd,buff,sizeof(buff)); //WRITE
-        if(strncmp("exit",buff,4)==0){
-            printf("\nServer Exits...");
-            break;
-        }
-    }
+    while(1){ 
+       bzero(buff,sizeof(buff)); 
+       printf("\nEnter the string : "); 
+       n=0; 
+       while((buff[n++]=getchar())!='\n'); 
+       write(sockfd,buff,sizeof(buff)); 
+       bzero(buff,sizeof(buff)); 
+       read(sockfd,buff,sizeof(buff)); 
+       printf("\nFrom server : %s",buff); 
+       if((strncmp(buff,"exit",4))==0) 
+       { 
+        printf("\nClient exit...\n"); 
+        break; 
+       } 
+     } 
 }
 
 int main(){
@@ -37,40 +37,22 @@ int main(){
         exit(0);
     }
     printf("Socket Successfully created");
-    
-    struct sockaddr serv;
-    
+
     //SERV PARAMETER ASSIGNING
+    struct sockaddr serv;
     bzero(&serv,sizeof(serv));
     serv.sin_family = AF_INET;
     serv.sin_ports = htons(PORT);
     serv.sin_adrs.s_addr = htonls(INADDR_ANY);
     
-    if(bind(sockfd,(struct sockaddr*)&serv, sizeof(serv)) != 0){
-        printf("Socket bind failed");
-        exit(0);
-    }
-    printf("Socket Successfully binded");
-    
-    //SERVER LISTENING
-    if((listen(sockfd,5)) != 0){
-        printf("Listen failed");
-        exit(0);
-    }
-    printf("Server listening")
-    
-    struct sockaddr cli;
-    len = sizeof(cli);
-    
-    //ACCEPT INCOMING CONNECTION
-    int connfd = accept(sockfd,(struct sockaddr)&cli, &len);
-    
-    if(connfd < 0){
-        printf("Server accept failed");
+    //CONNECT TO SERVER
+    int connfd = connect(sockfd,(struct sockaddr*)&serv, sizeof(serv));
+    if(connfd != 0){
+        printf("Connection Failed with Server ...\n");
         exit(0);
     }
     
-    printf("Server accepted the client");
+    printf("Connected to Server ...\n");
     func(connfd);
     close(sockfd);
     return 0;
